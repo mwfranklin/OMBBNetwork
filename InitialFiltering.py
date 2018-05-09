3208a76fe5224ac25bffdb7ed085069588d0b919
Meghan W Franklin <mwfranklin@ku.edu>
2018-05-09 15:03:21 -0500

    Updated duplicate PDB list

--- InitialFiltering.py
+++ InitialFiltering.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from collections import Counter
 
 all_pdb_ids = []
 with open("all.flat.d", "r") as in_data, open("rawData_E20_v6_2018.txt", "w") as outdata:
@@ -26,7 +27,15 @@ with open("all.flat.d", "r") as in_data, open("rawData_E20_v6_2018.txt", "w") as
             outdata.write("\t".join(line[:18]))# + "\t"+ "\t".join(line[21:]))
 
 all_pdb_ids = sorted(set(all_pdb_ids))
-for pdb_id in all_pdb_ids:
+all_pdb_ids = [x[0:4] for x in all_pdb_ids]
+possible_dups = Counter(all_pdb_ids)
+for value in possible_dups:
+    if possible_dups[value] != 1:
+        print(value)
+
+#Make incorrect fragment list from the printed statements above; also manually check entries in the rawdata file for sequence identities > 80%
+
+"""for pdb_id in all_pdb_ids:
     if os.path.isfile("PDBFiles/%s.pdb" %pdb_id) == True:
         print("File exists %s"%pdb_id)
     elif os.path.isfile("/Volumes/G_Drive/StructureAlign/NirBenTal/Joanna_and_Meghan/v4_July/pdb_files/%s.pdb"%pdb_id) == True:
@@ -37,5 +46,5 @@ for pdb_id in all_pdb_ids:
         shutil.copy("/Volumes/G_Drive/StructureAlign/NirBenTal/pdb70_29Apr17.pdb/%s.pdb"%pdb_id, "PDBFiles/%s.pdb"%pdb_id)
     else:
         print("Downloading %s" %pdb_id)
-        os.system("curl https://files.rcsb.org/download/%s.pdb -o PDBFiles/%s.pdb" %(pdb_id[0:4], pdb_id))
+        os.system("curl https://files.rcsb.org/download/%s.pdb -o PDBFiles/%s.pdb" %(pdb_id[0:4], pdb_id))"""
             
