e53e71ae7a0453e0c21fc9bda62e55f9580733a9
Meghan W Franklin <mwfranklin@ku.edu>
2018-07-17 15:04:35 -0400

    Figures from homologues

--- AddHomologues.py
+++ AddHomologues.py
@@ -1,5 +1,10 @@
 import os
 import re
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy import stats
+import numpy as np
+import textwrap
 import glob
 
 all_barrels = {}
@@ -21,7 +26,8 @@ for filename in glob.glob("MSAs/*.a3m"):
                 a3m_list.append(new_id)
     all_homologues[ pdb_id ] = a3m_list
 
-with open("data/AllDataE1_v6_Numbered.txt", "r") as inData, open("data/AllDataE1_v6_Numbered_Hom.txt", "w+") as outData, open("data/AllDataE1_v6_Numbered_Hom_Tabbed.txt", "w+") as outDataTab:
+homo_sizes = []
+with open("data/AllDataE1_v6_Numbered.txt", "r") as inData:#, open("data/AllDataE1_v6_Numbered_Hom.txt", "w+") as outData, open("data/AllDataE1_v6_Numbered_Hom_Tabbed.txt", "w+") as outDataTab:
     for line in inData:
         line = line.strip().split(" ")
         pdb1 = line[1]
@@ -30,10 +36,32 @@ with open("data/AllDataE1_v6_Numbered.txt", "r") as inData, open("data/AllDataE1
         if pdb1 in all_barrels.keys() and pdb2 in all_barrels.keys():
             all_homos = len(set(all_homologues[pdb1] + all_homologues[pdb2]))
             shared_homos = len(set(all_homologues[pdb1]).intersection(all_homologues[pdb2]))
+            homo_sizes.append(shared_homos/all_homos)
             #print(pdb1, pdb2, shared_homos/all_homos)
-            outData.write(" ".join(line) + " " + str(shared_homos/all_homos) + " "+ str(all_homos) + "\n")
-            outDataTab.write("\t".join(line) + "\t" + str(shared_homos/all_homos) + "\t" + str(all_homos) + "\n")
+            #outData.write(" ".join(line) + " " + str(shared_homos/all_homos) + " "+ str(all_homos) + "\n")
+            #outDataTab.write("\t".join(line) + "\t" + str(shared_homos/all_homos) + "\t" + str(all_homos) + "\n")
         else:
-            outData.write(" ".join(line) + " 0 0\n")
-            outDataTab.write("\t".join(line) + "\t0\t0\n")
+            continue
+            #outData.write(" ".join(line) + " 0 0\n")
+            #outDataTab.write("\t".join(line) + "\t0\t0\n")
+
+fig, ax3 = plt.subplots(nrows = 1, ncols = 1, figsize = (8,10))
+ax3.set_xlabel("Proportion of shared homologues", size = 22)
+ax3.set_ylabel("Probability", size = 22)
+kde = stats.gaussian_kde(homo_sizes)
+xnew = np.arange(0, 1, 0.01)
+ax3.plot(xnew, kde.evaluate(xnew), linewidth = 2)
+ax3.scatter(homo_sizes, np.full(len(homo_sizes), 0.005), c = "black", s = 50)
+ax3.hist(homo_sizes, bins = np.arange(0,1.1,0.1), histtype = "step", normed = True)
+#ax3.annotate("TEST", xy=[60,0.8], xycoords="data", fontsize=30)
+#leg = ax3.legend(fontsize = 14, loc = 2)
+# set the linewidth of each legend object
+#for legobj in leg.legendHandles:
+ #   legobj.set_linewidth(2.5)
+#ax3.set_ylim([0, 0.85])
+ax3.set_xlim([0,1])
+plt.tick_params(labelsize = 14)
+plt.tight_layout()
+plt.savefig("NetworkGraphs/DistribSharedHomologues.png")
+#plt.show()
             
