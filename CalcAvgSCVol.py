6ddb6eff331324fd4668dc845d0de3d26e91e521
Meghan W Franklin <mwfranklin@ku.edu>
2018-10-10 15:15:37 -0500

    Publication-ready SCVolumes png

--- CalcAvgSCVol.py
+++ CalcAvgSCVol.py
@@ -58,24 +58,25 @@ barrel_sizes = [8,10,12,14,16,18,22]
 
 #r2_labels = [98.5, 111] #for aa_avg
 #r2_labels = [101, 111] #for aa_avg_group
-r2_labels = [99, 109] #for noGly
-fig, ax1 = plt.subplots(nrows = 1, ncols= 1, figsize = (10,10))
+r2_labels = [99.5, 109] #for noGly
+fig, ax1 = plt.subplots(nrows = 1, ncols= 1, figsize = (7,7))
 ax1.scatter(barrel_sizes, new_avgs[:,0], c = "blue", marker = "o", label = "Inward", alpha=0.8)
 these_stats = scipy.stats.linregress(barrel_sizes, new_avgs[:,0])
-print(these_stats)
+#print(these_stats)
 ax1.plot(np.arange(6,25,0.5), these_stats.slope*np.arange(6,25,0.5) + these_stats.intercept, c = "blue")
-ax1.annotate("R2=%0.4f"%these_stats.rvalue**2, xy = (18.5, r2_labels[0]), xycoords = "data", size = 16)
+ax1.annotate(r"$\mathregular{R^2}$=%0.4f"%these_stats.rvalue**2, xy = (18.5, r2_labels[0]), xycoords = "data", size = 16)
 #print(these_stats.pvalue)
 ax1.scatter(barrel_sizes, new_avgs[:,1], c = "red", marker = "o", label = "Outward", alpha=0.8)
 these_stats = scipy.stats.linregress(barrel_sizes, new_avgs[:,1])
-print(these_stats)
+#print(these_stats)
 ax1.plot(np.arange(6,25,0.5), these_stats.slope*np.arange(6,25,0.5) + these_stats.intercept, c = "red")
-ax1.annotate("R2=%0.4f"%these_stats.rvalue**2, xy = (18.5, r2_labels[1]), xycoords = "data", size = 16)
+ax1.annotate(r"$\mathregular{R^2}$=%0.4f"%these_stats.rvalue**2, xy = (18.5, r2_labels[1]), xycoords = "data", size = 16)
 ax1.set_xlim([6,24])
-ax1.set_ylabel("Average Side-chain Volume, A", fontsize = 18)
-ax1.set_xlabel("Barrel Size", fontsize = 18)
-ax1.set_title("Average Side Chain Volumes", fontsize = 26)
+ax1.set_ylabel(r"Average Side Chain Volume ($\AA$)", fontsize = 18)
+ax1.set_xlabel("Barrel Size, Strands", fontsize = 18)
+ax1.set_title("Average Side Chain Volumes", fontsize = 24)
 plt.legend(fontsize = 18, loc = 4)
+plt.tick_params(labelsize = 14)
 plt.savefig("NetworkGraphs/SCVolumesBarrelAvg.png")
 
 #r2_labels = [98.5, 111] #for aa_avg
